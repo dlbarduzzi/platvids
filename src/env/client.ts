a9e4f618af1ecd23c30908bbbcb5f81fc4ed6994
Dylan Barduzzi <dlbarduzzi@gmail.com>
2025-02-16 20:18:02 -0600

    Add support email env variable

--- src/env/client.ts
+++ src/env/client.ts
@@ -4,6 +4,7 @@ import { createEnv } from "@t3-oss/env-nextjs"
 export const env = createEnv({
   client: {
     NEXT_PUBLIC_APP_URL: z.string().url(),
+    NEXT_PUBLIC_APP_EMAIL_SUPPORT: z.string().email(),
   },
   onValidationError: issues => {
     console.error("❌ Invalid client environment variables ❌", issues)
@@ -13,6 +14,7 @@ export const env = createEnv({
   runtimeEnv: {
     /* eslint-disable n/no-process-env */
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+    NEXT_PUBLIC_APP_EMAIL_SUPPORT: process.env.NEXT_PUBLIC_APP_EMAIL_SUPPORT,
     /* eslint-enable n/no-process-env */
   },
   emptyStringAsUndefined: true,
